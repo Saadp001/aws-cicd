40f86ac1ab0af141e40f427b0306e86e17109e82
Saadp001 <psaad2988@gmail.com>
2025-05-03 01:59:19 +0530

    final-push

--- first-aws-cicd/simple-python-app/start_container.sh
+++ first-aws-cicd/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull saadp200/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 saadp200/simple-python-flask-app
