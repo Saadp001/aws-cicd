221b68ada194d6812d69039754d9a5e50ceba8b3
Saadp001 <psaad2988@gmail.com>
2025-05-03 11:48:31 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull saadp200/simple-python-flask-app
+echo
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 saadp200/simple-python-flask-app
+echo
